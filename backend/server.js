d42c4e02581912e35856c8d3b34994d54143dbee
Lynn Varga <lynnkvarga@gmail.com>
2021-02-09 18:12:21 -0500

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+//Declarations
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 const app = express();
@@ -10,19 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Connect to MongoDB
 try {
     const uri = process.env.ATLAS_URI;
-//const uri = "mongodb+srv://dbAdmin:eRc09oXGHJQqXIwU@realmcluster.tze3d.mongodb.net/capstonedb?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+    mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");})
+    
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
 } catch (error) {
-    console.log("error:"+error);
+    console.log("Error:" + error);
 }
