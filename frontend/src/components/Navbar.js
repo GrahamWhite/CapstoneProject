5fdf531559fbb7f5e818c6c5ca362f07272b3825
Tyler Mills <tmills9208@conestogac.on.ca>
2021-02-16 14:09:15 -0500

    push

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,9 +22,13 @@ function Navbar() {
     <div className={classes.root}>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon/>
           </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
