bd0b6b03cb5b8681750e56365d74c0b0341b9264
Tyler Mills <tmills9208@conestogac.on.ca>
2021-03-11 13:53:14 -0500

    pushy push

--- frontend/src/components/GameList.js
+++ frontend/src/components/GameList.js
@@ -1,60 +1,111 @@
-import { Grid, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
+import React, { useEffect, useState, useCallback } from 'react'
+import SearchBar from "material-ui-search-bar"; // https://www.npmjs.com/package/material-ui-search-bar
 
-function GameItem({game, onUpdate}) {
-  const updateHandler = dummyData => onUpdate({ ...game, ...dummyData});
+function GameItem(props) {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      
+    },
+    cover: {
+      width: '151'
+    }
+  }));
+  const classes = useStyles();
+
+  const imgPlaceholder = "https://st3.depositphotos.com/13159112/17145/v/600/depositphotos_171453724-stock-illustration-default-avatar-profile-icon-grey.jpg";
+
+  // const updateHandler = dummyData => onUpdate({ ...props.game, ...props.dummyData});
+
+  const [selected, setSelected] = useState(false);
 
   return (
-    <Grid item container spacing={2}>
-      <Grid item xs={6}>
-        {game.name}
-      </Grid>
-      <Grid item xs={6}>
-        {game.platform}
-      </Grid>
-    </Grid>
+    <Card className={classes.card}
+      variant="outlined">
+      <CardActionArea
+        onClick = {() => {setSelected(!selected)}}>
+        <CardContent>
+          <CardMedia
+            className={classes.cover}
+            image={imgPlaceholder/*process.env.PUBLIC_URL + game.img*/}
+            />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                {props.game.name}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                {props.game.platform}
+              </Typography>
+            </CardContent>
+          </div>
+        </CardContent>
+      </CardActionArea>
+      {
+        selected ?
+        <CardActions>
+          <Button size="small" color="primary">
+            Favourite
+          </Button>
+          <Button size="small" color="red">
+            Delete
+          </Button>
+        </CardActions>
+        : ""
+      }
+      
+    </Card>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    
-  }
-}));
-
 function GameList(props) {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      
+    },
+    cover: {
+      width: '151'
+    }
+  }));
   const classes = useStyles();
 
   const dummyData = [{
     id: "1",
     name: "Apex Legends",
-    platform: "Steam, Origin"
+    platform: "Steam, Origin",
+    img: '/images/ApexLogo.png'
   },
   {
     id: "2",
     name: "Rainbow 6 Siege",
-    platform: "Steam, Ubisoft"
+    platform: "Steam, Ubisoft",
+    img: ''
   },
   {
     id: "3",
     name: "Deep Rock Galactic",
-    platform: "Steam"
+    platform: "Steam",
+    img: ''
   },
   {
     id: "4",
-    name: "as123",
-    platform: "asdasdzxc"
+    name: "Noita",
+    platform: "Steam",
+    img: ''
   },
   {
     id: "5",
-    name: "a1234",
-    platform: "asdazxcv"
+    name: "Polybius",
+    platform: "Steam, 80's Arcade Machine",
+    img: ''
   },
   ];
 
   const [games, setgames] = useState(dummyData);
+  const [selectedGame, setSelectedGame] = useState(null);
+  const [search, setSearch] = useState("");
 
-  const onChange = React.useCallback(updatedGame => {
+  const onChange = useCallback(updatedGame => {
     const gameIndex = games.findIndex(emp => emp.id === updatedGame.id)
     games[gameIndex] = updatedGame;
     setgames([...games]);
@@ -64,18 +115,16 @@ function GameList(props) {
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
+          <TableRow colSpan={2}>
+              {/* <SearchBar
+                value={search}
+              /> */}
           </TableRow>
         </TableHead>
         <TableBody>
           {games.map((game) => (
             <TableRow key={game.id}>
-              <TableCell component="th" scope="game" rowSpan={1}>
-                {game.name}
-              </TableCell>
-              <TableCell align="right">{game.platform}</TableCell>
-              <TableCell align="right">{game.platform}</TableCell>
+              <GameItem game={game}/>
             </TableRow>
           ))}
         </TableBody>
