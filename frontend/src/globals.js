af8814dad271840cb49c969fb9cee83e367567d8
Tyler Mills <tmills9208@conestogac.on.ca>
2021-03-20 15:37:33 -0400

    Added ReAuthenticate function

--- frontend/src/globals.js
+++ frontend/src/globals.js
@@ -1,1 +1,8 @@
+import { useHistory } from "react-router";
+
 export const backendURL = "http://ec2-35-183-39-123.ca-central-1.compute.amazonaws.com:3000";
+
+export const ReAuthenticate = () => {
+  localStorage.setItem('username', '');
+  useHistory.push('/login');
+}
