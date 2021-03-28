7b1d297883ee519aa45992d23509325e6a0ec972
Tyler Mills <tmills9208@conestogac.on.ca>
2021-03-28 15:58:03 -0400

    useHistory cannot be used outside of component

--- frontend/src/globals.js
+++ frontend/src/globals.js
@@ -1,8 +1,9 @@
-import { useHistory } from "react-router";
+
 
 export const backendURL = "http://ec2-35-183-39-123.ca-central-1.compute.amazonaws.com:3000";
 
-export const ReAuthenticate = () => {
+export const ReAuthenticate = (props) => {
   localStorage.setItem('username', '');
-  useHistory.push('/login');
+  props.history.push('/login');
 }
+
