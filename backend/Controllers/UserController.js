18d518f4b53b3f49618eb4ba5026cda8da385e36
Lynn Varga <lynnkvarga@gmail.com>
2021-03-28 17:17:14 -0400

    changed graham's bad error msg

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -48,7 +48,7 @@ const UpdateUser = async (req, res) => {
                 res.send("user updated");
             }
 
-            res.send("you fucked up");
+            res.send("Error: User is not defined");
         }
     }
     res.send("Error: username must be defined")
