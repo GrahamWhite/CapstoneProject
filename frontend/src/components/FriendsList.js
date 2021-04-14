d5681be259a2d87b7cf5a8a9ab55cbb43ede2e45
Lynn Varga <lynnkvarga@gmail.com>
2021-04-14 16:35:58 -0400

    Added avatars to friends

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,4 +1,5 @@
 import {
+  Avatar,
   Button,
   Card,
   CardActionArea,
@@ -48,6 +49,15 @@ function UserItem({ user, index, history, location, setRefresh, isProfile, isFri
       transform: "translate(-40px)",
       color: 'red',
       position: 'absolute',
+    },
+    friendIcon: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+      // backgroundColor: theme.palette.primary.main,
+      // color: theme.palette.primary.contrastText,
+
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.contrastText,
     }
   }));
   const classes = useStyles();
@@ -127,9 +137,16 @@ function UserItem({ user, index, history, location, setRefresh, isProfile, isFri
           />
           <div className={classes.details}>
             <CardContent className={classes.content}>
-              <Typography component="h5" variant="h5">
-                {user.username}
-              </Typography>
+            <Grid container item spacing={3}>
+                <Grid item xs={12} sm={1}>
+                  <Avatar className={classes.friendIcon}>{user.username.substr(0,2)}</Avatar>
+                </Grid>
+                <Grid item xs={12} sm={10}>
+                  <Typography component="h5" variant="h5">
+                  {user.username}
+                  </Typography>
+                </Grid>
+              </Grid>
             </CardContent>
           </div>
         </CardContent>
