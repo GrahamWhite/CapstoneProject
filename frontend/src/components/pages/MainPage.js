cc46d01ad8a8cf607b797158acd642ddf9d37a2b
Lynn Varga <lynnkvarga@gmail.com>
2021-04-16 10:51:56 -0400

    button styling

--- frontend/src/components/pages/MainPage.js
+++ frontend/src/components/pages/MainPage.js
@@ -14,7 +14,12 @@ const useStyles = makeStyles((theme) => ({
         width: '75%',
         margin: 'auto',
         marginBottom: '2%'
-    }
+    },
+    button:{
+      width: '25%',
+      paddingTop: '1%',
+      paddingBottom: '1%',
+    },
     }));
 
 function MainPage() {
@@ -29,15 +34,15 @@ function MainPage() {
           // onChange={(newValue) => this.setState({ value: newValue })}
           // onRequestSearch={() => doSomethingWith(this.state.value)}
           className={classes.center}/> */}
-          <Button className={classes.center} component={Link} to={"/Help"} variant="contained" color="secondary">Learn how to use Tink</Button>
+          <Button className={`${classes.center} ${classes.button}`} component={Link} to={"/Help"} variant="contained" color="secondary">Learn how to use Tink</Button>
           <Typography variant="body1" className={classes.center} style={{color:"#666", marginTop:"2.5%"}}>To see your library of games, friends, and edit your profile:</Typography>
-          <Button className={classes.center} component={Link} to={"/profile"} variant="contained" color="primary">See your profile</Button>
+          <Button className={`${classes.center} ${classes.button}`} component={Link} to={"/profile"} variant="contained" color="primary">See your profile</Button>
           <Typography variant="body1" className={classes.center} style={{color:"#666", marginTop:"2.5%"}}>To find a new friend, view a profile or match with a user:</Typography>
-          <Button className={classes.center} component={Link} to={"/search"} variant="contained" color="primary">Search for a user</Button>
+          <Button className={`${classes.center} ${classes.button}`} component={Link} to={"/search"} variant="contained" color="primary">Search for a user</Button>
           <Typography variant="body1" className={classes.center} style={{color:"#666", marginTop:"2.5%"}}>View your added users, match with your friends:</Typography>
-          <Button className={classes.center} component={Link} to={"/friend"} variant="contained" color="primary">See your friends list</Button>
+          <Button className={`${classes.center} ${classes.button}`} component={Link} to={"/friend"} variant="contained" color="primary">See your friends list</Button>
           <Typography variant="body1" className={classes.center} style={{color:"#666", marginTop:"2.5%"}}>Add games to your library:</Typography>
-          <Button className={classes.center} component={Link} to={"/games"} variant="contained" color="secondary">Manage your library</Button>
+          <Button className={`${classes.center} ${classes.button}`} component={Link} to={"/games"} variant="contained" color="secondary">Manage your library</Button>
     </div>
   )
 }
