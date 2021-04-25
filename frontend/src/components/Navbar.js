be5372e19ba34825779ed7426d14d6fd03b9b02c
Lynn Varga <lynnkvarga@gmail.com>
2021-04-24 21:36:11 -0400

    nav button remove

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -33,9 +33,9 @@ function Navbar() {
     <div className={classes.root}>
       <AppBar position='fixed'>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             {/* <MenuIcon/> */}
-          </IconButton>
+          {/* </IconButton> */}
           <Typography variant="h6" className={classes.title}>
             Tink
           </Typography>
